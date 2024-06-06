96b13e9ef8a6c88978ea5beadd657ff723fe574f
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-06-06 22:54:20 +0900

    테ìassert, test환경 ë활성화

---
+++ src/main/java/com/ki/level0/p120811/Main.java
@@ -0,0 +1,15 @@
+package com.ki.level0.p120811;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+
+}
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        return answer;
+    }
+}
