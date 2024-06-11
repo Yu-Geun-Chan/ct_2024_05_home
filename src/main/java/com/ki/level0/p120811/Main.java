51d8bc058dd13a45a0626228cfc3709e57fba038
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-06-11 23:09:53 +0900

    프로그래머스 p120811무문제

--- src/main/java/com/ki/level0/p120811/Main.java
+++ src/main/java/com/ki/level0/p120811/Main.java
@@ -1,5 +1,8 @@
 package com.ki.level0.p120811;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -7,8 +10,28 @@ public class Main {
     }
 
 }
+
 class Solution {
     public int solution(int[] array) {
-        return 17;
+        int a;
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    a = array[i];
+                    array[i] = array[j];
+                    array[j] = a;
+                }
+            }
+        }
+        for (int i = 0; i <= array.length - 1; i++) {
+            if (i == (array.length )/ 2) {
+                return array[i];
+            }
+        }
+
+        return 0;
     }
+
 }
+
+
